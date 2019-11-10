29a360d1a9384150bb2bcc8f6744cc7514663f00
sbx_user1051 <sbx_user1051@169.254.7.125>
2019-11-10 09:37:34 +0000

    Sun Nov 10 09:37:34 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,6 +2,8 @@
 def compute(x, y):
     if x<0 or x > 100 or y<0 or y>100:
         raise ValueError(x,'Input parameter out of bounds')
+    if y<0 or y>100:
+        raise ValueError(y,'Input parameter out of bounds')
     return x+y
-    # raise NotImplementedError()
+
 
