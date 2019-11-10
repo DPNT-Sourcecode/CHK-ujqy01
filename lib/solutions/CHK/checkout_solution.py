f7789c84a61b65adda888d4ccfb3c07f92e61875
sbx_user1051 <sbx_user1051@169.254.39.245>
2019-11-10 13:00:11 +0000

    Sun Nov 10 13:00:11 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -14,10 +14,10 @@ def checkout(skus):
                   {'name': 'C', 'price': 20,'on_offer': False},
                   {'name': 'D', 'price': 15,'on_offer': False}]
 
-    offers_list = [{'id':'0','type':'multibuy','offer_item':'A','offer_quantity':3,'offer_price':130},
-                   {'id':'1','type':'multibuy','offer_item':'A','offer_quantity':5,'offer_price':200},
+    offers_list = [{'id':'0','type':'multibuy','offer_item':'A','offer_quantity': 3,'offer_price': 130},
+                   {'id':'1','type':'multibuy','offer_item':'A','offer_quantity': 5,'offer_price': 200},
                    {'id': '2','type':'multibuy', 'offer_item': 'B', 'offer_quantity': 2, 'offer_price': 45},
-                   {'id': '2', 'type': 'multibuy', 'offer_item': 'B', 'offer_quantity': 2, 'offer_price': 45},
+                   {'id': '2', 'type': 'buy_one_get_another', 'offer_item': 'E', 'offer_quantity': 2, 'free_item': 'B'},
                    ]
     total_price = 0
 
@@ -33,7 +33,7 @@ def checkout(skus):
         price_list_item = next((item for item in price_list if item["name"] == sku), None)
 
         if price_list_item is not None:
-            if price_list_item.get('on_offer'):
+            if price_list_item.get('on_offer') and price_list_item.get('offer_type') == 'multibuy':
                 on_offer_items_count = skus.count(sku)
                 offer_price = math.trunc(on_offer_items_count / price_list_item.get('offer_quantity')) * price_list_item.get('offer_price')
                 non_offer_price = on_offer_items_count%price_list_item.get('offer_quantity') * price_list_item.get('price')
@@ -41,6 +41,9 @@ def checkout(skus):
                 price = offer_price + on_offer_items_count%price_list_item.get('offer_quantity') * price_list_item.get('price')
 
                 skus = list(filter(lambda a: a != sku, skus))
+            elif price_list_item.get('on_offer') and price_list_item.get('offer_type') == 'free_item':
+                on_offer_items_count = skus.count(sku)
+                on_offer_items_count
             else:
                 price = price_list_item.get('price')
 
@@ -48,5 +51,6 @@ def checkout(skus):
         total_price += price
         print('total_price',total_price)
     return total_price
+
 
 
