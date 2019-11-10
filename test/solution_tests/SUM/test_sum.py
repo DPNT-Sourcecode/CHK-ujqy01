1395683eb7803263b8743f389fe9d9b11746fdff
sbx_user1051 <sbx_user1051@169.254.7.125>
2019-11-10 09:30:26 +0000

    Sun Nov 10 09:30:26 UTC 2019

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,10 +1,28 @@
 from solutions.SUM import sum_solution
 
 
+
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
 
     def test_sum_parameter_out_of_bounds(self):
-       self.assertRaises(sum_solution.compute(101, 2))
+       self.assertRaises(InputError,sum_solution.compute(101, 2))
+
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+
+class InputError(Error):
+    """Exception raised for errors in the input.
+
+    Attributes:
+        expression -- input expression in which the error occurred
+        message -- explanation of the error
+    """
+
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = 'Input parameter(s) out of bounds'
 
