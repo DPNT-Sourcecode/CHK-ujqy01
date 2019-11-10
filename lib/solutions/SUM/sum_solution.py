6284efa7df2e6961d439dccdf133e0ee7b6170ce
sbx_user1051 <sbx_user1051@169.254.7.125>
2019-11-10 09:27:26 +0000

    Sun Nov 10 09:27:26 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,5 +1,8 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
+    if x<0 or x > 100 or y<0 or y>100:
+        raise
     return x+y
     # raise NotImplementedError()
+
 
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -4,3 +4,7 @@ from solutions.SUM import sum_solution
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
+    def test_sum_parameter_out_of_bounds(self):
+       self.assertRaises(sum_solution.compute(101, 2))
+
