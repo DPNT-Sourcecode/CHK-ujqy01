b2d701d595bf8b215286760a299ab6d24cbf4179
sbx_user1051 <sbx_user1051@169.254.7.125>
2019-11-10 10:16:55 +0000

    Sun Nov 10 10:16:55 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,15 +6,19 @@ import string
 def checkout(skus):
     # alphabet = string.ascii_uppercase
     alphabet = 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'.split()
-    price_list = [{'name': 'A', 'price': 50},{'B':30},{'C':20},{'D':15}]
-
+    price_list = [{'name': 'A', 'price': 50},{'name': 'B', 'price':30},{'name': 'C', 'price': 20},{'name': 'D', 'price': 15}]
+    total_price = 0
     skus = skus.split()
     for sku in skus:
         if sku not in alphabet:
             return -1
-        next((item for item in price_list if item["name"] == sku), None)
+        price_list_item = next((item for item in price_list if item["name"] == sku), None)
+        if price_list_item is not None:
+            price = price_list_item.get('price',0)
+        # print(price)
+        total_price += price
+    return total_price
 
-    return 0
 
 
 
