7e045378b070f76bc7e7770ee50704d608659f31
sbx_user1051 <sbx_user1051@169.254.7.125>
2019-11-10 10:01:55 +0000

    Sun Nov 10 10:01:55 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,10 +4,12 @@ import string
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    alphabet = string.ascii_uppercase.split()
-    print(alphabet)
+    # alphabet = string.ascii_uppercase
+    alphabet = 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'
+    print(alphabet.split())
     SKUs = skus.split()
 
     return -1
+
 
 
