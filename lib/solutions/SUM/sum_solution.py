5dbf7bbb6b8ca2c36027139e0f4370fdf2ee4ff9
sbx_user1051 <sbx_user1051@169.254.7.125>
2019-11-10 09:36:26 +0000

    Sun Nov 10 09:36:26 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,24 +1,7 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
     if x<0 or x > 100 or y<0 or y>100:
-        raise InputError(x,'Input parameter out of bounds')
+        raise ValueError(x,'Input parameter out of bounds')
     return x+y
     # raise NotImplementedError()
 
-
-class Error(Exception):
-    """Base class for exceptions in this module."""
-    pass
-
-
-class InputError(Error):
-    """Exception raised for errors in the input.
-
-    Attributes:
-        expression -- input expression in which the error occurred
-        message -- explanation of the error
-    """
-
-    def __init__(self, expression, message):
-        self.expression = expression
-        self.message = message
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -7,22 +7,5 @@ class TestSum():
         assert sum_solution.compute(1, 2) == 3
 
     def test_sum_parameter_out_of_bounds(self):
-       with pytest.raises(InputError):
+       with pytest.raises(ValueError):
            sum_solution.compute(101, 2)
-
-class Error(Exception):
-    """Base class for exceptions in this module."""
-    pass
-
-
-class InputError(Error):
-    """Exception raised for errors in the input.
-
-    Attributes:
-        expression -- input expression in which the error occurred
-        message -- explanation of the error
-    """
-
-    def __init__(self, expression, message):
-        self.expression = expression
-        self.message = 'Input parameter(s) out of bounds'
