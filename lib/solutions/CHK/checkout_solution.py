50dd451148bb7908daeed243684ba2fc35b598d9
sbx_user1051 <sbx_user1051@169.254.7.125>
2019-11-10 10:13:55 +0000

    Sun Nov 10 10:13:55 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,14 +6,15 @@ import string
 def checkout(skus):
     # alphabet = string.ascii_uppercase
     alphabet = 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'.split()
-    price_list = [{'A': 50},{'B':30},{'C':20},{'D':15}]
+    price_list = [{'name': 'A', 'price': 50},{'B':30},{'C':20},{'D':15}]
 
     skus = skus.split()
     for sku in skus:
         if sku not in alphabet:
             return -1
-        if
+        next((item for item in price_list if item["name"] == sku), None)
 
     return 0
+
 
 
